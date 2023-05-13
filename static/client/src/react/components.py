f0944e2be8623c77cb610858421876f50ddb63c4
Atai Ambus <atayambus@gmail.com>
2023-05-13 11:02:04 +0300

    Remove react

--- static/client/src/react/components.py
+++
@@ -1,9 +0,0 @@
-from src.react.component import Component
-
-import js.MaterialUI as mui
-class Button(Component):
-    _wrapped = mui.Button
-
-
-def __getattr__(tag_name: str):
-    return type(tag_name, (Component,), {'_wrapped': tag_name})
